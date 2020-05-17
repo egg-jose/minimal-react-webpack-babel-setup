70d003edc8d62de0c8738e04caefaa807d8ad686
Jose Esteban <josegg1897@gmail.com>
2020-05-17 16:18:13 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Installation
+
+```Bash
+git clone git@github.com:jose1897/minimal-react-webpack-babel-setup.git
+cd minimal-react-webpack-babel-setup
+npm install
+npm start
+```
+<b>visit `http://localhost:3000/`</b>
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebpackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html",
+});
+
+module.exports = {
+  entry: "./src",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  plugins: [htmlPlugin],
+};
